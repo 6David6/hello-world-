4e43ea772743c7ecf325ba54d470b2b882d1db94
6David6 <100133895+6David6@users.noreply.github.com>
2022-03-21 15:22:52 +0800

    Add files via upload

---
+++ Hello world!/Hello world!/practice.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("My name is David.\n");
+    printf("I use this website for the first time.\n");
+    printf("At present, I'm just a rookie.\n");
+    printf("Thank you!\n");
+    return 0;
+}
